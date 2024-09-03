2e02ea4e8ace5fe4ff12293ea944daae836a5980
Monica Saha <146354383+monica-saha@users.noreply.github.com>
2024-09-03 13:41:49 +0000

    typeconversion

---
+++ JSbasic/typeconversion.js
@@ -0,0 +1,26 @@
+let score = 33
+console.log(typeof score)
+
+let age = "29"
+console.log(typeof age)
+
+let point="23ssd"
+console.log(point)
+console.log(typeof point)
+
+let newpoint =Number(point)
+console.log(newpoint)
+console.log(typeof newpoint)
+
+let cricket=null
+console.log(typeof cricket)
+
+let newcricket = String(cricket)
+console.log(typeof newcricket)
+
+let name="monica 233"
+console.log(name)                                        //nan = not a number
+
+let newname= Number(name)
+console.log(newname)
+console.log(typeof newname)
