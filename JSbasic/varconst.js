32dcb282a397a48259517fb04f596208f258cea4
Monica Saha <146354383+monica-saha@users.noreply.github.com>
2024-09-03 11:35:44 +0000

    variable and constant

---
+++ JSbasic/varconst.js
@@ -0,0 +1,12 @@
+const accountId = 2300446                        // cannot change value of constant
+var accountEmail="monica@gmai.com"               // var keyword is not use nowadays because of scope issue
+let accountPassword = "moon"                     // let keyword is used for variable in JS to initilize variable
+
+let accountrest                                  // accountrest is name of variable
+
+    // const is use for constant and let is use for variable that can be change
+    //; is not necessary
+
+console.log(accountId)                        //print output on screen for single variable
+console.table([accountEmail,accountId,accountPassword,accountrest]) //print output for multiple variable in form of table
+
